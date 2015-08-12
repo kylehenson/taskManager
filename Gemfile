24933e225fc42b3e4f234cdc4b0aa7d00142eb2e
Kyle Henson <khenson.kh@gmail.com>
2015-08-11 21:44:31 -0600

    Create, Read, and Update built for task

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
   gem 'byebug'
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'launchy'
   gem 'web-console', '~> 2.0'
 end
 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,45 @@
 class TasksController < ApplicationController
 
-  def index
-
+  def new
+    @list = List.find(params[:list_id])
+    @task = Task.new
   end
-  
+
+  def create
+    list = List.find(params['list_id'])
+    @task = list.tasks.new(task_params)
+    if @task.save
+      flash[:notice] = "Task successfully created."
+      redirect_to list_path(list)
+    else
+      flash[:errors] = @task.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:notice] = "successfully updated task"
+      redirect_to list_path(@task.list_id)
+    else
+      flash[:errors] = @task.errors.full_messages.join(', ')
+      render :edit
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title,
+                                 :notes,
+                                 :status,
+                                 :start_date,
+                                 :due_date)
+  end
+
 end
